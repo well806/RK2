865caea5e16b9bd125eaf90d4b820b35775ffe22
well806 <ridigervalentin@yandex.ru>
2024-12-20 16:53:29 +0300

    Update rk2.cpp

--- rk2/rk2.cpp
+++ rk2/rk2.cpp
@@ -1,10 +1,27 @@
-﻿
-
 #include <iostream>
+#include <bitset>
+#include <string>
 using namespace std;
-int main()
-{
-	cout << "Hello World!\n";
+
+int main() {
+    string A_str = "101101";
+    string B_str = "110011";
+
+
+    int A = stoi(A_str, nullptr, 2); 
+    int B = stoi(B_str, nullptr, 2);  
+
+    
+    int and_result = A & B;
+
+    int sum = A + B;
+
+ 
+    cout << "A (d): " << A << ", A (b): " << bitset<6>(A) << endl;
+    cout << "B (d): " << B << ", B (b): " << bitset<6>(B) << endl;
+
+    cout << "AND result: " << bitset<6>(and_result) << " (b), " << and_result << " (d)" << endl;
+    cout << "Sum result: " << bitset<7>(sum) << " (b), " << sum << " (d)" << endl;
+
+    return 0;
 }
-
-
